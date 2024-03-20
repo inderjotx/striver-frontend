da8e93e2f9136c70d919aea1bb05a273c17e527d
Inderjot Singh <inderjotsingh141@gmail.com>
2024-03-21 00:57:59 +0530

    home page desing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { NAVLINKS } from '@/constant'
+import { Github } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
@@ -8,7 +9,7 @@ import React from 'react'
 
 export function Navbar() {
     return (
-        <nav className='flex justify-center h-20  items-center'>
+        <nav className='flex  relative h-20  justify-center items-center'>
             <div className='relative flex px-8 bg-muted text-sm items-center gap-3 h-10 rounded-full'>
                 {
                     NAVLINKS.map(({ name, href }) => (
@@ -18,6 +19,10 @@ export function Navbar() {
                     ))
                 }
             </div>
+            <a href={'https://github.com/inderjotx/striver-frontend'} className='absolute right-8' target='_blank' >
+                <Github className='size-6 '></Github>
+            </a>
+
         </nav>
     )
 }
