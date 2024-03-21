1e052d1ae9afb9ff486b4aace398b0d2d74c89d5
Inderjot Singh <inderjotsingh141@gmail.com>
2024-03-21 09:56:13 +0530

    view user code : feat

--- components/DrawerViewCode.tsx
+++ components/DrawerViewCode.tsx
@@ -1,4 +1,3 @@
-
 import * as React from "react"
 import {
     Drawer,
@@ -7,9 +6,6 @@ import {
 } from "@/components/ui/drawer"
 import { MonacoEditor } from "./CodeEditor/editor"
 import { Button } from "./ui/button"
-
-
-
 
 interface DrawerButtonProps extends React.HTMLProps<HTMLDivElement> {
     language: SupportedLanguages,
@@ -30,10 +26,8 @@ export function DrawerViewCode({ language, code, height, width, children, classN
             </DrawerTrigger>
             <DrawerContent className="h-[500px]  items-center ">
                 <div className="flex mt-2 flex-col h-[450px] overflow-hidden rounded-md w-10/12">
-
                     <MonacoEditor language={language} code={code} setCode={(val: string) => { }} height={height} width={width} className={`${className}`} readOnly />
                     <div className="w-1/3">
-
                     </div>
                 </div>
             </DrawerContent>
